2bfd0ee0a50e3d1d04f237868c6be62f5257b0fa
O Park <o@ohsikpark.com>
2023-07-16 13:08:50 -0400

    clean up thinkorswim data-transform

--- lib/brokerages/thinkorswim/data-transform.ts
+++ lib/brokerages/thinkorswim/data-transform.ts
@@ -8,57 +8,57 @@ import { Data, Leg, Option, Stock } from "../../types";
   Data is the data format we want to use in the application
 */
 
+function getOrderDirection(instruction: string): Option["direction"] {
+  const debitOrders = ["BUY", "BUY_TO_COVER", "BUY_TO_CLOSE"];
+  return debitOrders.includes(instruction) ? 'debit' : 'credit';
+}
+
+type GetSideAndPositionEffectReturn = {
+  side: Leg["side"];
+  positionEffect: Leg["positionEffect"];
+} | undefined;
+
+function getSideAndPositionEffect(instruction: string): GetSideAndPositionEffectReturn {
+  // TODO: find out if BUY and SELL orders are needed here
+  // filtering them out for now by returning undefined
+
+  // BUY, SELL, BUY_TO_COVER, SELL_SHORT, BUY_TO_OPEN, BUY_TO_CLOSE, SELL_TO_OPEN, SELL_TO_CLOSE, EXCHANGE
+
+  const conversionMap: Record<string, GetSideAndPositionEffectReturn> = {
+    'BUY_TO_COVER': {
+      side: 'buy',
+      positionEffect: 'close'
+    },
+    'SELL_SHORT': {
+      side: 'sell',
+      positionEffect: 'open'
+    },
+    'BUY_TO_OPEN': {
+      side: 'buy',
+      positionEffect: 'open'
+    },
+    'BUY_TO_CLOSE': {
+      side: 'buy',
+      positionEffect: 'close'
+    },
+    'SELL_TO_OPEN': {
+      side: 'sell',
+      positionEffect: 'open'
+    },
+    'SELL_TO_CLOSE': {
+      side: 'sell',
+      positionEffect: 'close'
+    },
+  };
+
+  return conversionMap[instruction]; // BUY, SELL, EXCHANGE will be undefined and everything else that are not defined here
+}
+
 export function dataTransform(fetchedData: any): Data {
   // console.log({fetchedData})
 
   let stocks: Stock[] = [];
   let options: Option[] = [];
-
-  function getOrderDirection(instruction: string): Option["direction"] {
-    const debitOrders = ["BUY", "BUY_TO_COVER", "BUY_TO_CLOSE"];
-    return debitOrders.includes(instruction) ? 'debit' : 'credit';
-  }
-
-  type GetSideAndPositionEffectReturn = {
-    side: Leg["side"];
-    positionEffect: Leg["positionEffect"];
-  } | undefined;
-
-  function getSideAndPositionEffect(instruction: string): GetSideAndPositionEffectReturn {
-    // TODO: find out if BUY and SELL orders are needed here
-    // filtering them out for now by returning undefined
-
-    // BUY, SELL, BUY_TO_COVER, SELL_SHORT, BUY_TO_OPEN, BUY_TO_CLOSE, SELL_TO_OPEN, SELL_TO_CLOSE, EXCHANGE
-
-    const conversionMap: Record<string, GetSideAndPositionEffectReturn> = {
-      'BUY_TO_COVER': {
-        side: 'buy',
-        positionEffect: 'close'
-      },
-      'SELL_SHORT': {
-        side: 'sell',
-        positionEffect: 'open'
-      },
-      'BUY_TO_OPEN': {
-        side: 'buy',
-        positionEffect: 'open'
-      },
-      'BUY_TO_CLOSE': {
-        side: 'buy',
-        positionEffect: 'close'
-      },
-      'SELL_TO_OPEN': {
-        side: 'sell',
-        positionEffect: 'open'
-      },
-      'SELL_TO_CLOSE': {
-        side: 'sell',
-        positionEffect: 'close'
-      },
-    };
-  
-    return conversionMap[instruction]; // BUY, SELL, EXCHANGE will be undefined and everything else that are not defined here
-  }
 
   fetchedData.data.orders.forEach((order: any) => {
     if(order.orderLegCollection[0].orderLegType === 'EQUITY') {
